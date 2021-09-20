e9ba9c4d17151d55ed16fb96441f8da813c48526
DSITD-Wadudi <DSITD-Wadudi@LAPTOP-8T15LAOC>
2021-09-20 12:51:26 +0700

    Deteksi Karakter pakai algoritme Hebb

---
+++ Hebb_Deteksi-Huruf.R
@@ -0,0 +1,47 @@
+
+
+data_heb <- read.csv("C:/Users/DSITD-Wadudi/Documents/data dudi/dokumen/S2 ILKOM/Semester 1/Kecerdasan Komputasional/Kuliah/fonts-heb.csv", header = T)
+#View(data_heb)
+datalatih_heb <- data_heb[1:14,1:63]
+#View(datalatih_heb)
+
+#buat target 
+target_heb_datalatih <- c(1,-1,1,1,-1,-1,1,1,-1,1,1,-1,-1,1)
+
+#awal
+bobot_heb <- as.vector(rep(0, 63))
+bias_heb <- c(1,1,1,1,1,1,1,1,1,1,1,1,1,1)
+
+fungsi_aktivasi <- as.vector(rep(0, 14))
+
+# update bobot baru
+# w1 = w1old + (x1* t)
+
+# update bias baru
+# b1 = blama + t
+
+nepoch <- 1
+
+n <- nrow(datalatih_heb)
+final_target_datalatih <- as.vector(rep(0, n))
+
+
+for(obyek in 1:n){
+  
+  bias_heb[obyek] <- bias_heb[obyek] + target_heb_datalatih[obyek]
+  
+  for(w in 1:63){
+    bobot_heb[w] <- bobot_heb[w] + (datalatih_heb[obyek,w] * target_heb_datalatih[obyek])
+  }
+   
+}
+
+for(obyek in 1:n){
+  for(w in 1:63){
+    fungsi_aktivasi[obyek] <- fungsi_aktivasi[obyek] + (datalatih_heb[obyek,w] * bobot_heb[w])
+  }
+  fungsi_aktivasi[obyek] + fungsi_aktivasi[obyek] + bias_heb[14]
+}
+
+print(fungsi_aktivasi)
+
